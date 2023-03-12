aafbe9acc24c0af29e09324087c8c70dd28b8089
danwoo <danwoo@lbox.kr>
2023-03-12 17:05:57 +0900

    Initial commit

---
+++ src/test/kotlin/com/danwoo/fastcampusissueservice/FastcampusIssueServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.danwoo.fastcampusissueservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastcampusIssueServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
